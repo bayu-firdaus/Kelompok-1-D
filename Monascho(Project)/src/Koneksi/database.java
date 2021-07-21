0cfba1360f576ab9ad9274cd5905c6b8d90b8acd
AriefAnas1 <80686758+AriefAnas1@users.noreply.github.com>
2021-07-21 09:21:58 +0700

    Arief Anastiar Suharsono
    Project Kelompok 1

---
+++ Monascho(Project)/src/Koneksi/database.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Kelompok 1
+ */
+public class database {
+  Connection koneksi=null;
+  
+      public Connection getKoneksi(){
+        return koneksi;
+    }
+  
+     public static Connection koneksiDB(){
+ try { 
+     Class.forName("com.mysql.jdbc.Driver");
+    Connection koneksi=DriverManager.getConnection("jdbc:mysql://localhost:3306/monascho","root","");
+       return koneksi;
+    }catch(Exception e) {
+    JOptionPane.showMessageDialog(null, e);
+     return null;
+    }
+ }       
+}
